fe31820bc855842be1475c3cd5f08924f67834e3
Sage-Kearney <56275819+Sage-Kearney@users.noreply.github.com>
2020-03-04 12:43:45 -0500

    created django app. server is running-booyah

---
+++ selfies_django/selfies/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class SelfiesConfig(AppConfig):
+    name = 'selfies'
---
+++ selfies_django/settings.sql
@@ -0,0 +1,3 @@
+CREATE DATABASE selfies;
+CREATE USER selfiesuser WITH PASSWORD 'selfies';
+GRANT ALL PRIVILEGES ON DATABASE selfies TO selfiesuser;
